13f831eb2666e14b2e3eeb0969c783c96a150843
jackarohan <jackarohan@gmail.com>
2025-08-13 05:52:50 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 window.DASH_CONFIG = {
   g_trend: 0.018,                 // long-run real GDP trend (decimal)
   defaultRates: 'FRED',           // FRED JSON by default
-  fredApiKey: "299e8ed99ca79ba973bb5ad9078beed6",  // your key
+  fredApiKey: '21dcbd8202ef8e0e05f91b1e8faeb417',  // your key
   preferFredJson: true,
   samples: {
     yieldXML: 'samples/yield.xml',
